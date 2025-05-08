6b7f732954e4d0754ebb219dfcb19e5b0d3d0c50
Derk-B <74244634+Derk-B@users.noreply.github.com>
2025-05-08 23:12:59 +0200

    New SoA works

--- common/scheduler/performance_counters.cc
+++ common/scheduler/performance_counters.cc
@@ -137,14 +137,10 @@ double PerformanceCounters::getTemperatureOfComponent (string component) const {
 double PerformanceCounters::getTemperatureOfCore(int coreId) const {
     string prefix = "C_" + std::to_string(coreId) + "_";
     vector<double> temperatures = getValues(instTemperatureFileName, prefix);
-    cout << "temperatures.size() = " << temperatures.size() << endl;
     if (temperatures.size() == 0) {
         return -1;
     }
-    cout << "temperatures.size() = " << temperatures.size() << endl;
-    double res = *std::max_element(temperatures.begin(), temperatures.end());
-    cout << "temperatures.size() = " << temperatures.size() << endl;
-    return res;
+    return *std::max_element(temperatures.begin(), temperatures.end());
 }
 
 /**
